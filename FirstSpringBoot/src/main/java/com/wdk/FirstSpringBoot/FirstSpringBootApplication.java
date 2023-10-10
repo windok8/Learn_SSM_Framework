29bb19d8484a82604c66d42e0a9488329c942d39
windok8 <54491035+windok8@users.noreply.github.com>
2023-10-10 15:59:30 +0800

    First Commit

---
+++ FirstSpringBoot/src/main/java/com/wdk/FirstSpringBoot/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wdk.FirstSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+
+}
